83b3fcb63c6d8619e992b06516df6ed43c45dc34
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-04-18 23:16:07 +1000

    Fixed issue where contentHost was defined as integer rather than text

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,7 +3,7 @@ USE `nbdl`;
 CREATE TABLE `urls` (
     id INT NOT NULL PRIMARY KEY,
     contentId TEXT NOT NULL UNIQUE,
-    contentHost INT NOT NULL,
+    contentHost TEXT NOT NULL,
     dateCreated DATE NOT NULL,
     dateExpire DATE NOT NULL,
     contentFileName TEXT NOT NULL
