7449c95a299f61ddbc9c0deafdd80c8879db47e5
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-04-20 01:38:07 +1000

    Added code to ignore file extentinons and serve the file to the user

--- cdn/index.js
+++ cdn/index.js
@@ -1,3 +1,32 @@
 const express = require("express");
-const cfg = require("./config.json")
+const cfg = require("./config.json");
+const mysql = require("mysql");
 
+var app = express();
+
+var con = mysql.createConnection({
+  host: cfg.sql.host,
+  user: cfg.sql.user,
+  password: cfg.sql.password,
+  database: cfg.sql.database
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.get("/*", function(req, res) {
+    console.log(req.url.replace("/", "").split(".")[0]);
+    con.query(`SELECT * FROM \`urls\` WHERE \`contentId\`="${req.url.replace("/", "").split(".")[0]}"`, function (err, result, fields, app_res=res) {
+        if (err) throw err;
+        if (result.length > 0) {
+          res.sendFile(result[0]["contentFileName"]);
+        } else {
+          res.statusCode = 404; // Not found
+          res.send("<html><span>404 Not Found</span></html>");
+        }
+      });
+})
+
+app.listen(3000);
