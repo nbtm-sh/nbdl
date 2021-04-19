72d35b3bd1450432ca7e8b7c1eef56ccfd29f2ec
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-04-20 02:56:39 +1000

    Added password option to allow the cdn to connect

--- sql/create_users.sql
+++ sql/create_users.sql
@@ -1,7 +1,7 @@
 START TRANSACTION;
 USE `nbdl`;
-CREATE USER IF NOT EXISTS 'nbdl'@'localhost' IDENTIFIED BY 'default_JNMt$523FmeMJE^S'; 
-CREATE USER IF NOT EXISTS 'nbdlexpire'@'localhost' IDENTIFIED BY 'default_S5#cj&QfH8~S5xkr';
+CREATE USER IF NOT EXISTS 'nbdl'@'localhost' IDENTIFIED WITH mysql_native_password BY 'default_JNMt$523FmeMJE^S'; 
+CREATE USER IF NOT EXISTS 'nbdlexpire'@'localhost' IDENTIFIED WITH mysql_native_password BY 'default_S5#cj&QfH8~S5xkr';
 USE `nbdl`;
 GRANT INSERT, SELECT ON `urls` TO 'nbdl'@'localhost';
 GRANT INSERT, UPDATE, DELETE, SELECT ON `urls` TO 'nbdlexpire'@'localhost';
